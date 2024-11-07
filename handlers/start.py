d1d8812fb80b34d1f433805e4fd53b01c34bfa8e
9098787 <obmen79obmen@gmail.com>
2024-11-07 20:48:49 +0200

    added start handler

--- handlers/start.py
+++ handlers/start.py
@@ -1,3 +1,14 @@
 from aiogram import Router, F
+from aiogram.types import Message, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 router = Router()
+
+@router.message(F.text == '/start')
+async def start(message: Message):
+    builder = InlineKeyboardBuilder()
+    builder.add(InlineKeyboardButton(text='📝Зарегистрироваться', callback_data='registration'))
+
+    await message.answer('<b>Добро пожаловать!\nДля использования бота необходимо пройти регистрацию</b>',
+                         reply_markup=builder.as_markup(),
+                         parse_mode='html')
